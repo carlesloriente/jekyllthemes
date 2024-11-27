989261bc86fb27e6d109616d41a116bd2aab564e
Carles Loriente <7197733+carlesloriente@users.noreply.github.com>
2024-11-27 02:07:46 +0100

    This is it!

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 WEBROOT="docs"
-OUTPUT_DIR="assets/vendor"
 
 build_site() {
   JEKYLL_ENV=production bundle exec jekyll build --incremental --verbose --trace --destination ${WEBROOT} --config _config.yml;
-  mkdir -p ${WEBROOT}/${OUTPUT_DIR};
-  rsync -avz --progress ${INPUT_DIR} ${WEBROOT}/${OUTPUT_DIR} --exclude src;
 }
 
 build_site;
